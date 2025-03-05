e4728b1d4fe6340206d482a0faf529a75661f644
KingZhang8675 <313056427@qq.com>
2025-03-05 23:16:29 +0800

    1223

--- components/at/src/at_cmd_register.c
+++ components/at/src/at_cmd_register.c
@@ -92,10 +92,13 @@ ESP_AT_CMD_SET_FIRST_INIT_FN(esp_at_driver_cmd_regist, 19);
 
 #ifdef CONFIG_AT_ETHERNET_SUPPORT
 ESP_AT_CMD_SET_FIRST_INIT_FN(esp_at_eth_cmd_regist, 20);
+<<<<<<< HEAD
+=======
 
+>>>>>>> 09aadd0b0a83c582f57eb2b694c35d223b97a5d3
 #endif
-
 ESP_AT_CMD_SET_INIT_FN(esp_at_custom_cmd_register, 21);
+
 
 void esp_at_cmd_set_register(void)
 {
